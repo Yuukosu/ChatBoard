8c56a000fa6d5536b6cd64a472b5374ff797275d
Yuukosu <yuuworks0926@gmail.com>
2023-01-29 03:47:45 +0900

    Updated

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
 	echo "Usage: $0 [MySQL Host]"
 	exit 0
 fi
 
-mysql -u root -h $1 --password=root < init.sql
+if [ -z "which mysql" ]; then
+	echo "MySQL がインストールされていません。"
+	exit 0
+fi
+
+mysql -u root -h $1 --password=root < ./init.sql
--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+
+sudo -v
+
 if [ -z "$(ls -d ./database)" ]; then
 	mkdir database
 fi
 
-docker-compose up -d
+if [ -z "$(which docker-compose)" ]; then
+	echo "docker-compose がインストールされていません。"
+	exit 0
+elif [ -z "$(which docker)" ]; then
+	echo "docker がインストールされていません。"
+	exit 0
+fi
+
+sudo chown $(whoami):$(whoami) -R ./database
+docker-compose up --build -d
